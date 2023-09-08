6e9b00496a75eb2e05ac28ae8d84eaa323e5701a
Ali Osman Ilık <72392747+alioss64@users.noreply.github.com>
2023-09-08 12:15:17 +0300

    Add files via upload

---
+++ hafta5.py
@@ -0,0 +1,24 @@
+# Bir örnek dizi
+dizi = [12, 45, 67, 23, 89, 34, 50]
+
+# Dizinin içindeki en büyük sayıyı bulmak için max() fonksiyonunu kullanabilirsiniz.
+en_buyuk_sayi = max(dizi)
+
+# Sonucu yazdırın
+print("En büyük sayı:", en_buyuk_sayi)
+
+#------------------------------------------------------------------------------------------#
+
+# Bir örnek dizi oluşturun
+dizi = [12, 45, 67, 23, 89, 34, 50]
+
+# Dizinin içindeki en büyük sayıyı bulun
+en_buyuk_sayi = max(dizi)
+
+# Dizinin içindeki en küçük sayıyı bulun
+en_kucuk_sayi = min(dizi)
+
+# Sonuçları ekrana yazdırın
+print("En büyük sayı:", en_buyuk_sayi)
+print("En küçük sayı:", en_kucuk_sayi)
+ 
